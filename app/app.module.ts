30c825dca0aa0c488adca5ceb088672284d1c3e8
eeasss <eeasss@gmail.com>
2016-09-13 19:03:59 +0300

    feat: added routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
 
+import { DashboardComponent } from  './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent } from './app.component';
+
+import { routing } from './app.routing';
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        routing
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
+        HeroesComponent,
         HeroDetailComponent
     ],
     bootstrap: [ AppComponent ]
